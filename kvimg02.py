99fe632a68afa5ee7e5a807bd2e210ea397c8263
chrissi <christina.rost@hfg-gmuend.de>
2019-01-09 15:34:31 +0100

    animated transition: rgb - grey - blur

--- kvimg02.py
+++ kvimg02.py
@@ -41,18 +41,10 @@ Builder.load_string('''
 
 <ImgAnim>:
 
-    Image:
-        id: blur
-        pos: 10, root.pos_y - 538/2
-        size: 778, 583
-        source: 'images/orig.JPEG'
-        allow_stretch: False
-        keep_ratio: True
-        pos_hint: {'center_x', 'center_y'}
 
     Image:
         id: grey
-        pos: 200, root.pos_y - 538/2
+        pos: 10, root.pos_y - 538/2
         size: 778, 583
         source: 'images/img_grey.JPEG'
         allow_stretch: False
@@ -60,8 +52,27 @@ Builder.load_string('''
         pos_hint: {'center_x', 'center_y'}
 
     Image:
+        id: blur
+        pos: 10, root.pos_y - 538/2
+        size: 778, 583
+        source: 'images/blur.JPG'
+        allow_stretch: False
+        keep_ratio: True
+        pos_hint: {'center_x', 'center_y'}
+
+    Image:
+        id: grey2
+        pos: 10, root.pos_y - 538/2
+        size: 778, 583
+        source: 'images/img_grey.JPEG'
+        allow_stretch: False
+        keep_ratio: True
+        pos_hint: {'center_x', 'center_y'}
+
+
+    Image:
         id: alpha
-        pos: 200, root.pos_y - 538/2
+        pos: 10, root.pos_y - 538/2
         size: 778, 583
         source: 'images/orig.JPEG'
         allow_stretch: False
@@ -115,38 +126,33 @@ class ImgAnim(Widget):
     def move_ani(self):
         print("animating")
         Animation.cancel_all(self)
-        target_x = Window.width/2 - self.children[0].width/2
+        target_x = 800
 
         ani = Animation(x=target_x, duration=1, t='in_out_sine')
         ani.start(self.ids['grey'])
         ani.start(self.ids['alpha'])
+        ani.start(self.ids['blur'])
+        ani.start(self.ids['grey2'])
 
     def alpha_ani(self):
         ani2 = Animation(opacity=0, duration=1, t='in_out_sine')
         ani2.start(self.ids['alpha'])
 
     def move_blur_ani(self):
-        ani3 = Animation(x=Window.width/2, duration=1, t='in_out_sine')
+        ani3 = Animation(x=Window.width-900, duration=1, t='in_out_sine')
         ani3.start(self.ids['blur'])
-        time.sleep(1)
+        ani3.start(self.ids['grey2'])
+        # time.sleep(1)
 
-        # arr = np.array(self.ids['blur'].texture)
-        arr = np.array(Image('images/orig.JPEG').texture)
-        buf = cv2.blur(arr, (5, 5))
-        texture = Texture.create(size=(778, 583))
-        texture.blit_buffer(buf, colorfmt='rgba', bufferfmt='ubyte')
-
-        self.ids['blur'].texture = texture
+    def alpha_blur(self):
+        ani4 = Animation(opacity=0, duration=1, t='in_out_sine')
+        ani4.start(self.ids['grey2'])
 
     def pyr_ani(self):
-        
-
-
-
-
+        pass
 
     aniNum = 0
-    ani_dict = {0:move_ani , 1:alpha_ani, 2:move_blur_ani}
+    ani_dict = {0:move_ani , 1:alpha_ani, 2:move_blur_ani, 3:alpha_blur}
     ani_iter = iter(ani_dict.items())
 
     def on_touch_down(self, touch):
@@ -156,8 +162,7 @@ class ImgAnim(Widget):
             ani = self.ani_iter.__next__()[1]
             ani(self)
         except StopIteration:
-            ani = iter(self.ani_dict.items())
-            self.on_touch_down(self, touch)
+            pass
         self.aniNum +=1
 
 
