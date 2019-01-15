9161eb51862915b1021ff9210753e547f5937f6c
Christina Rost <christina.rost@hfg-gmuend.de>
2019-01-15 15:32:27 +0100

    removed error (whitespaces) in kv file. renamed kv file, no builder required

--- faceDetectKivy.py
+++ faceDetectKivy.py
@@ -1,47 +1,35 @@
 # coding: utf-8
-
-# fullscreen
 from kivy.config import Config
 Config.set('graphics', 'fullscreen', 'auto')
 
-from kivy.lang import Builder
+import cv2
+import numpy as np
+# fullscreen
+from imutils import face_utils, paths
+from imutils.object_detection import non_max_suppression
+from imutils.video import FPS, WebcamVideoStream
+from kivy.app import App
+from kivy.clock import Clock
 
 from kivy.core.window import Window
-from kivy.properties import ObjectProperty, ListProperty, StringProperty, NumericProperty
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.scatter import Scatter
-
-
-from kivy.app import App
-from kivy.uix.image import Image
-from kivy.uix.slider import Slider
-from kivy.clock import Clock
 from kivy.graphics.texture import Texture
-from imutils.video import WebcamVideoStream
-from imutils.video import FPS
-from imutils import face_utils
-import numpy as np
-import cv2
+from kivy.lang import Builder
+from kivy.properties import (ListProperty, NumericProperty, ObjectProperty,
+                             StringProperty)
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.image import Image
+from kivy.uix.scatter import Scatter
+from skimage.draw import circle
+
 import dlib
 
-from skimage.draw import circle
-from imutils.object_detection import non_max_suppression
-from imutils import paths
 
-Builder.load_file('faceDetect.kv')
-# Builder.load_string('''
-# <MainWindow>
-#     orientation: 'vertical'
-#     ActionBar:
-#         id: actBar
-#         pos_hint: {'top':1}
-#         Slider:
-#             id: Opera
-# ''')
-#
+
+
+
+
 # class MainWindow(FloatLayout):
 #     pass
-
 
 
 class KivyCamera(Image):
@@ -55,7 +43,8 @@ class KivyCamera(Image):
         # self.videostream = capture
         self.videostream = WebcamVideoStream().start()
         self.detector = dlib.get_frontal_face_detector()
-        self.predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+        self.predictor = dlib.shape_predictor(
+            "shape_predictor_68_face_landmarks.dat")
 
         self.hog = cv2.HOGDescriptor()
         self.hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
@@ -64,7 +53,8 @@ class KivyCamera(Image):
         # Clock.schedule_interval(self.update, 1.0/fps)
 
     def morphology_transform(self, img, morph_operator=2, element=1, ksize=18):
-        morph_op_dic = {0: cv2.MORPH_OPEN, 1: cv2.MORPH_CLOSE, 2: cv2.MORPH_GRADIENT, 3: cv2.MORPH_TOPHAT, 4: cv2.MORPH_BLACKHAT}
+        morph_op_dic = {0: cv2.MORPH_OPEN, 1: cv2.MORPH_CLOSE,
+                        2: cv2.MORPH_GRADIENT, 3: cv2.MORPH_TOPHAT, 4: cv2.MORPH_BLACKHAT}
 
         if element == 0:
             morph_elem = cv2.MORPH_RECT
@@ -73,7 +63,9 @@ class KivyCamera(Image):
         elif element == 2:
             morph_elem = cv2.MORPH_ELLIPSE
 
-        elem = cv2.getStructuringElement(morph_elem, (2 * ksize + 1, 2* ksize + 1), (ksize, ksize))
+        elem = cv2.getStructuringElement(morph_elem,
+                                         (2 * ksize + 1, 2 * ksize + 1),
+                                         (ksize, ksize))
         operation = morph_op_dic[morph_operator]
         dst = cv2.morphologyEx(img, operation, elem)
 
@@ -83,7 +75,8 @@ class KivyCamera(Image):
         # vals = [self.to_parent(self.circle_y, self.circle_x), self.circle_r]
         vals = [[self.circle_y, self.circle_x], self.circle_r]
         print("circle vals in KvCam: {}".format(vals))
-        print("circle toparent in KvCam: {}".format(self.to_parent(self.circle_y, self.circle_x)))
+        print("circle toparent in KvCam: {}".format(
+            self.to_parent(self.circle_y, self.circle_x)))
         return vals
 
     def update(self):
@@ -95,7 +88,6 @@ class KivyCamera(Image):
         faces = self.detector(gray, 1)
         morphed_frame = self.morphology_transform(frame.copy())
 
-
         for (i, face) in enumerate(faces):
             # shape = self.predictor(gray, face)
             # shape = face_utils.shape_to_np(shape)
@@ -103,14 +95,12 @@ class KivyCamera(Image):
             # for (x, y) in shape:
             #     cv2.circle(frame, (x, y), 1, (0, 255, 0), -1)
 
-            self.circle_r = (w + w/2) / 2
-            circle_y = x + w/2
-            circle_x = y + h/2
-
+            self.circle_r = (w + w / 2) / 2
+            circle_y = x + w / 2
+            circle_x = y + h / 2
 
             rr, cc = circle(circle_x, circle_y, self.circle_r, frame.shape)
             morphed_frame[rr, cc] = frame[rr, cc]
-
 
             #  TODO:
             # flip :)
@@ -144,6 +134,7 @@ class KivyCamera(Image):
     def stop(self):
         self.videostream.stop()
 
+
 class Rings(Scatter):
     radius = NumericProperty(10)
     update_pos = True
@@ -153,23 +144,26 @@ class Rings(Scatter):
     #     if self.update_pos:
     #         self.pos, self.radius = vals
     #         self.pos = self.pos[0] - self.radius, self.pos[1] - self.radius
-    #         # self.pos = [Window.width - self.pos[0], Window.height - self.pos[1]]
+    #         # self.pos = [Window.width - self.pos[0],
+    #                       Window.height - self.pos[1]]
     #         print("circle in rings: {}".format(self.pos))
+
 
 class Screen(FloatLayout):
 
-        def __init__(self, **kwargs):
-            super(Screen, self).__init__(**kwargs)
-            self.fps = FPS().start()
+    def __init__(self, **kwargs):
+        super(Screen, self).__init__(**kwargs)
+        self.fps = FPS().start()
 
+        Clock.schedule_interval(self.update, 1.0 / 30)
 
-            Clock.schedule_interval(self.update, 1.0/30)
+    def update(self, dt):
+        print("screen ids: {}".format(self.ids))
+        self.ids.cam.update()
 
-        def update(self, dt):
-            self.ids.cam.update()
+    def destroy(self):
+        self.ids.cam.stop()
 
-        def destroy(self):
-            self.ids.cam.stop()
 
 class CamApp(App):
     def build(self):
@@ -178,6 +172,7 @@ class CamApp(App):
 
     def on_stop(self):
         self.screen.destroy()
+
 
 if __name__ == '__main__':
     CamApp().run()
